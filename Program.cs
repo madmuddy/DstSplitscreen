5e0dbc23b0cd9ab241ad842ae9525c9ae7338bdd
madmuddy <mmamma496@gmail.com>
2021-10-01 04:25:24 -0700

    Initilize

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
+namespace DSTSplitScreen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process.Start("UniversalSplitScreen.exe");
+
+            Thread.Sleep(10000);
+
+            string confPath = Path.Combine(Directory.GetCurrentDirectory(), @"steam_settings\settings");
+
+            using (StreamWriter sw = new StreamWriter(confPath + @"\user_steam_id.txt"))
+            {
+                sw.Write("76561199582812500");
+                sw.Close();
+            }
+
+            Process.Start("dontstarve_steam.exe");
+
+            Thread.Sleep(20000);
+
+            using (StreamWriter sw = new StreamWriter(confPath + @"\user_steam_id.txt"))
+            {
+                sw.Write("76561199582812501");
+                sw.Close();
+            }
+
+            Process.Start("dontstarve_steam.exe");
+        }
+    }
+}
